1b57383aa96ae69e7e15e966a12ef6bb45e2fe4b
Pedro Henrique <phpo2022@ufmg.br>
2024-11-05 16:59:12 -0300

    Add render method and HTTP status codes

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,20 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
     # Carrega o template polls/index.html e passa um contexto
 
 def detail(request, question_id):
-    return HttpResponse("Detalhando a questão de ID %s." % question_id)
-
-def results(request, question_id):
-    response = "Resultados da questão de ID %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("Votando para a questão %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/details.html", {"question": question})
