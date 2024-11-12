7f556538cd354eca0ed5790d3e2ddb21bcc24167
Pedro Henrique <phpo2022@ufmg.br>
2024-11-12 15:07:15 -0300

    Finish first basic index-vote-results poll with manually written, loose-coupled views.

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views # importa views da pasta acima
+from . import views
 
+app_name = "polls" # namespace
 urlpatterns = [
 	path("", views.index, name="index"),
 	path("<int:question_id>/", views.detail, name="detail"),
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,43 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
     # Carrega o template polls/index.html e passa um contexto
 
 def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/details.html", {"question": question})
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Exibe novamente o form
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Você não fez uma escolha.",
+            },
+            )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
